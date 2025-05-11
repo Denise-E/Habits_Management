3478d881de2940efe611f744caebdce2f12b7e9c
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2025-05-11 18:01:50 -0300

    logging config added

---
+++ backend/utils/logger.py
@@ -0,0 +1,18 @@
+import logging
+
+
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    return logger
+
+
+# Configura el logger
+logger = setup_logger()
